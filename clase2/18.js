39ecb5856477075a57141ea49c04f018205d81e7
francoariel1492 <francoariel1492@gmail.com>
2022-12-21 16:29:03 -0300

    diapositiva 18 - object keys,values,entries

---
+++ clase2/18.js
@@ -0,0 +1,11 @@
+const persona = {
+    name: "Mate",
+    apellido: "Naran",
+    edad: 3,
+    country: 'Colombia'
+}
+
+console.log(persona)
+console.log(Object.keys(persona))
+console.log(Object.values(persona))
+console.log(Object.entries(persona))
